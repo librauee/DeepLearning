78647941cbb649bee5c1d820b4e35fa03b292a99
librauee <870407139@qq.com>
2019-04-16 10:42:08 +0800

    建立一个神经网络

--- neural_network.py
+++ neural_network.py
@@ -17,13 +17,32 @@ class neuralNetwork(object):
         self.lr=learningrate
         #self.wih=np.random.rand(self.hnodes,self.inodes)-0.5
         #self.who=np.random.rand(self.onodes,self.hnodes)-0.5
+        #means,Variance,shape of array
         self.wih = np.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
         self.who = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+        #sigmoid function
         self.activation_function=lambda x:special.expit(x)
         pass
     
     #train the neural network
-    def train():
+    def train(self,inputs_list,targets_list):
+        #convert inputs list and targets list  to 2d array
+        inputs=np.array(inputs_list,ndmin=2).T
+        targets=np.array(targets_list,ndmin=2).T
+        
+        hidden_inputs = np.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)        
+        # calculate signals into final output layer
+        final_inputs = np.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+        output_errors=targets-final_outputs
+        hidden_errors=np.dot(self.who.T,output_errors)
+        self.who +=self.lr*np.dot((output_errors*final_outputs*(1.0-final_outputs)),
+                                 np.transpose(hidden_outputs))
+        self.wih +=self.lr*np.dot((hidden_errors*hidden_outputs*(1.0-hidden_outputs)),
+                                 np.transpose(inputs))
         pass
     
     
@@ -51,9 +70,10 @@ output_nodes=3
 
 learning_rate=0.3
 n=neuralNetwork(input_nodes,hidden_nodes,output_nodes,learning_rate)
+n.train([1,0.5,-0.5],[0.8,0.6,0.6])
+print(n.query([1,0.5,-0.5]))
 
-
-
+'''
 a1=np.random.rand(3,3)  #0-1的随机值
 a2=a1-0.5               #-0.5-0.5的随机值
-
+'''
